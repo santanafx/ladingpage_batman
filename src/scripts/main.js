6f83fe3af417ffc95a8d4643205d3c44042d0cae
Lucas Santana <santanafx@hotmail.com>
2023-08-03 21:47:03 -0300

    finalizado parte mobile

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,7 @@
-console.log("ola");
+const menu = document.querySelector(".menu__container__iconMenu");
+const menuDropdown = document.querySelector(".menu__container__dropdown");
+
+menu.addEventListener("click", function () {
+    const classe = "menu__container__iconMenu--is-active";
+    menuDropdown.classList.toggle(classe);
+});
